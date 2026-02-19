3dfa4524b18eadcd9330a6f63a9bd71a081ae595
Hamidou ismaila raouf RAIMI <raouf.raimi@chem.loc>
2026-02-19 12:27:42 +0100

    inscription rectifier

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,27 +1,49 @@
 import { ref } from "vue";
 
 export const authService = {
-    login(email, password){
-        if (email === 'admin@admin.com' && password === '123456') {
+   
+    login(email, password) {
+      
+        const users = JSON.parse(localStorage.getItem('users') || '[]');
+        
+    
+        const userFound = users.find(u => u.email === email && u.password === password);
+
+       
+        if ((email === 'admin@admin.com' && password === '123456') || userFound) {
             localStorage.setItem('auth', 'true');
-            auth.value = true
+            auth.value = true;
             return true;
-        }return false
+        }
+        return false;
     },
 
-     logout(){
-        localStorage.removeItem('auth')
-        auth.value = false
+    logout() {
+        localStorage.removeItem('auth');
+        auth.value = false;
     },
 
-  inscription(userData) {
-    console.log('new user:', userData)
 
-    return true
-  },
-}
+    inscription(userData) {
+       
+        const users = JSON.parse(localStorage.getItem('users') || '[]');
+        
+        if (users.find(u => u.email === userData.email)) {
+            alert("Cet email est déjà utilisé");
+            return false;
+        }
+
+       
+        users.push(userData);
+        localStorage.setItem('users', JSON.stringify(users));
+        
+        console.log('Utilisateur enregistré avec succès !');
+        return true;
+    },
+};
+
 export const isAuthenticated = () => {
-  return localStorage.getItem('auth') === 'true'
-}
+    return localStorage.getItem('auth') === 'true';
+};
 
-export const auth = ref(localStorage.getItem('auth') === 'true')
+export const auth = ref(localStorage.getItem('auth') === 'true');
